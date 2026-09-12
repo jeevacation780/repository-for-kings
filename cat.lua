c060f39478ebd66524ef4e9a5f89101db063c74f
jeevacation780 <jeevacationary@gmail.com>
2026-09-12 23:13:31 +0100

    Update cat.lua

--- cat.lua
+++ cat.lua
@@ -8018,7 +8018,7 @@ AntisTab:CreateToggle({
 (function()
     if not _G.UNLOCK_ANTICHEAT then return end
     AntisTab:CreateSection('Anticheat')
-    --AntisTab:CreateLabel('This is completely unrelated to forsaken anticheat! it is a client-sided checking mechanism to detect other cheaters and notify you.')
+    AntisTab:CreateLabel('If you lag alot if may falsely accuse players, especially teleportation and stamina mods.')
     local cheaters = {}
     local cheatersnames = {}
     local anticheaterrors = {}
